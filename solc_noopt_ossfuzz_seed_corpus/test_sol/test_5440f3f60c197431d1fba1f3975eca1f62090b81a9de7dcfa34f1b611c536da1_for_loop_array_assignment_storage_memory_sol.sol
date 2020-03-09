1dcd650746590e8501870f3ad8d81fe7be326042
Bhargava Shastry <bhargava.shastry@ethereum.org>
2020-03-09 11:03:08 +0100

    Updated solidity seed files via solidity isolate_tests.py on 2020-03-09

---
+++ solc_noopt_ossfuzz_seed_corpus/test_sol/test_5440f3f60c197431d1fba1f3975eca1f62090b81a9de7dcfa34f1b611c536da1_for_loop_array_assignment_storage_memory_sol.sol
@@ -0,0 +1,23 @@
+pragma experimental SMTChecker;
+
+contract LoopFor2 {
+	uint[] b;
+	uint[] c;
+
+	function testUnboundedForLoop(uint n) public {
+		b[0] = 900;
+		uint[] memory a = b;
+		require(n > 0 && n < 100);
+		for (uint i = 0; i < n; i += 1) {
+			b[i] = i + 1;
+			c[i] = b[i];
+		}
+		// This is safe but too hard to solve currently.
+		assert(b[0] == c[0]);
+		assert(a[0] == 900);
+		assert(b[0] == 900);
+	}
+}
+// ----
+// Warning: (316-336): Assertion violation happens here
+// Warning: (363-382): Assertion violation happens here
---
+++ solc_noopt_ossfuzz_seed_corpus/test_sol/test_bad41702c5a38eb00f7408665f4f0db0df780fb71bed3ed4a3fb824804873cd8_for_loop_array_assignment_storage_storage_sol.sol
@@ -0,0 +1,24 @@
+pragma experimental SMTChecker;
+
+contract LoopFor2 {
+	uint[] b;
+	uint[] c;
+
+	function testUnboundedForLoop(uint n) public {
+		b[0] = 900;
+		uint[] storage a = b;
+		require(n > 0 && n < 100);
+		for (uint i = 0; i < n; i += 1) {
+			b[i] = i + 1;
+			c[i] = b[i];
+		}
+		// This is safe but too hard to prove currently.
+		assert(b[0] == c[0]);
+		assert(a[0] == 900);
+		assert(b[0] == 900);
+	}
+}
+// ----
+// Warning: (317-337): Assertion violation happens here
+// Warning: (341-360): Assertion violation happens here
+// Warning: (364-383): Assertion violation happens here
