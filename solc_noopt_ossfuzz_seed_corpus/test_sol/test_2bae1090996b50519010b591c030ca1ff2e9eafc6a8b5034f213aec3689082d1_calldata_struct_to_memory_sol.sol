b972b9762c081406539fc302609b1a15b4adac4c
Bhargava Shastry <bhargava.shastry@ethereum.org>
2020-08-12 13:30:50 +0200

    Updated solidity seed files via solidity isolate_tests.py on 2020-08-12

---
+++ solc_noopt_ossfuzz_seed_corpus/test_sol/test_2bae1090996b50519010b591c030ca1ff2e9eafc6a8b5034f213aec3689082d1_calldata_struct_to_memory_sol.sol
@@ -0,0 +1,18 @@
+pragma experimental ABIEncoderV2;
+
+contract C {
+struct S {
+    uint256 a;
+    uint256 b;
+}
+
+function f(S calldata s) external pure returns (uint256, uint256) {
+    S memory m = s;
+    return (m.a, m.b);
+}
+}
+
+// ====
+// compileViaYul: also
+// ----
+// f((uint256,uint256)): 42, 23 -> 42, 23
---
+++ solc_opt_ossfuzz_seed_corpus/test_sol/test_a3b28a7ced07920a22764a8c0187cae0ad8374dd5fa7bf836d1a691d9c149cd2_while_loop_array_assignment_storage_storage_sol.sol
@@ -0,0 +1,28 @@
+pragma experimental SMTChecker;
+
+contract LoopFor2 {
+	uint[] b;
+	uint[] c;
+
+	function testUnboundedForLoop(uint n) public {
+		b[0] = 900;
+		uint[] storage a = b;
+		require(n > 0 && n < 100);
+		uint i;
+		while (i < n) {
+			b[i] = i + 1;
+			c[i] = b[i];
+			++i;
+		}
+		// Fails as false positive.
+		assert(b[0] == c[0]);
+		assert(a[0] == 900);
+		assert(b[0] == 900);
+	}
+}
+// ====
+// SMTSolvers: cvc4
+// ----
+// Warning 4661: (296-316): Assertion violation happens here
+// Warning 4661: (320-339): Assertion violation happens here
+// Warning 4661: (343-362): Assertion violation happens here
