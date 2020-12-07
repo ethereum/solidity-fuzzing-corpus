b00e0e864fff24753baa1bdc3a8ee4a9deab8ad2
Bhargava Shastry <bhargava.shastry@ethereum.org>
2020-12-07 12:17:05 +0100

    Updated solidity seed files via solidity isolate_tests.py on 2020-12-07

---
+++ solc_noopt_ossfuzz_seed_corpus/test_sol/test_493eae659e5fd57db7280f2b0432d44a3b30d87575461d33addd2e68e292fc80_v2_v1_v1_modifier_sandwich_sol.sol
@@ -0,0 +1,30 @@
+==== Source: A ====
+pragma abicoder               v2;
+
+struct Data {
+bool flag;
+}
+
+contract A {
+function get() public view returns (Data memory) {}
+}
+==== Source: B ====
+import "A";
+
+contract B {
+modifier validate() {
+    A(address(0x00)).get();
+    _;
+}
+}
+==== Source: C ====
+import "B";
+
+contract C is B {
+function foo()
+    public
+    validate()
+{}
+}
+// ----
+// TypeError 2428: (B:60-82): The type of return parameter 1, struct Data, is only supported in ABI coder v2. Use "pragma abicoder v2;" to enable the feature.
