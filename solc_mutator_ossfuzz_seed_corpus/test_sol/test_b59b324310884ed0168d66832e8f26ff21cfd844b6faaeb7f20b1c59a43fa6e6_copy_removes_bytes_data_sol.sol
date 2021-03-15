a54494f2dc9705824747238400f7092a4121d858
Bhargava Shastry <bhargava.shastry@ethereum.org>
2021-03-15 15:37:39 +0100

    Updated solidity seed files via solidity isolate_tests.py on 2021-03-15

---
+++ solc_mutator_ossfuzz_seed_corpus/test_sol/test_b59b324310884ed0168d66832e8f26ff21cfd844b6faaeb7f20b1c59a43fa6e6_copy_removes_bytes_data_sol.sol
@@ -0,0 +1,17 @@
+
+contract c {
+function set() public returns (bool) { data1 = msg.data; return true; }
+function reset() public returns (bool) { data1 = data2; return true; }
+bytes data1;
+bytes data2;
+}
+// ====
+// compileViaYul: also
+// ----
+// set(): 1, 2, 3, 4, 5 -> true
+// gas irOptimized: 163861
+// gas legacy: 163756
+// gas legacyOptimized: 163595
+// storage: nonempty
+// reset() -> true
+// storage: empty
